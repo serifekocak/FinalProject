39d21d230f1dca3d421e568cbf837973cf476b7e
serifekocak <sserifekocakk@gmail.com>
2025-01-10 16:09:04 +0300

    Day 12 - Part 1

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,8 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.AutoFac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntitiyFramework;
 
@@ -11,8 +14,15 @@ builder.Services.AddControllers();
 
 // IoC container için eklendi
 // Biri constructorda IProductService nesnesi isterse ona bir ProductManager nesnesi ver (newlenir)
-builder.Services.AddSingleton<IProductService, ProductManager>();
-builder.Services.AddSingleton<IProductDal, EfProductDal>();
+//builder.Services.AddSingleton<IProductService, ProductManager>();
+//builder.Services.AddSingleton<IProductDal, EfProductDal>();
+
+// .Net içerisinde bulunan IoC container yerine AutoFac kullanmasýný söylüyoruz.
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+    .ConfigureContainer<ContainerBuilder>(options =>
+    {
+        options.RegisterModule(new AutofacBusinessModule());
+    });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
